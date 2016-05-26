078df179dd85c0392791c143d69f3dd83abc2860
Lisa Klein <lisa.klein@rtl.nl>
2016-05-26 14:30:27 +0200

    routing

--- static/modules/routes.js
+++ static/modules/routes.js
@@ -5,32 +5,32 @@ var sections = (function() {
                 '*': function(){
                     sections.toggle(window.location.hash);
                 },
-			    'homePage': function() {	
+			    'homePage': function() {
 			    	sections.toggle(window.location.hash);
 			    },
 			    'myTimetable': function() {
-			    	sections.toggle(window.location.hash);			    	  
+			    	sections.toggle(window.location.hash);
 			    },
 			    'showEvents': function() {
 			    	sections.toggle(window.location.hash);
 			    }
 			});
-		
+
 		};
 
 		var toggle = function(hashName) {
-			var allSections = document.querySelectorAll('section');	
-					
+			var allSections = document.querySelectorAll('.toggleSection');
+
 			for (var i = 0; i < allSections.length; i++) {
 				allSections[i].classList.add('inactive');
-                
+
                 if (!hashName) {
                     allSections[0].classList.remove('inactive');
 
                 } else {
                     document.querySelector(hashName).classList.remove('inactive');
                 }
-			}		
+			}
 		};
 
 	return {
