386f4ea57dec8eda84e42d01367083a7a6ffc0a2
Lisa Klein <lisa.klein@planet.nl>
2016-05-27 15:17:27 +0200

    working on loading data server side + getting it into templates.. //not finished

--- public/js/modules/templating.js
+++ public/js/modules/templating.js
@@ -1,18 +1,18 @@
 var templating = (function () {
 
     var init = function (hasStorage) {
-        var showEvents = document.getElementById('showEvents').innerHTML
-
-        if (hasStorage) {
-            var data = JSON.parse(localStorage.getItem('allEventsData'));
-        }
-        else{
-            console.log("There is no localStorage detected.");
-        }
-
-        document.getElementById('showEvents').innerHTML = Mustache.render(showEvents, {
-            data: data
-        });
+//        var showEvents = document.getElementById('showEvents').innerHTML
+//
+//        if (hasStorage) {
+//            var data = JSON.parse(localStorage.getItem('allEventsData'));
+//        }
+//        else{
+//            console.log("There is no localStorage detected.");
+//        }
+//
+//        document.getElementById('showEvents').innerHTML = Mustache.render(showEvents, {
+//            data: data
+//        });
 
     }
 
