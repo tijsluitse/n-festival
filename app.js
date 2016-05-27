1960bd34c256f1acc8191f695824373007187e8a
Tijs Luitse <tijs.luitse@gmail.com>
2016-05-27 12:51:49 +0200

    update json data

--- app.js
+++ app.js
@@ -1,50 +1,3 @@
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
-//
-//
-//// uncomment after placing your favicon in /public
-////app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-
-//// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//  var err = new Error('Not Found');
-//  err.status = 404;
-//  next(err);
-//});
-//
-//// error handlers
-//
-//// development error handler
-//// will print stacktrace
-//if (app.get('env') === 'development') {
-//  app.use(function(err, req, res, next) {
-//    res.status(err.status || 500);
-//    res.render('error', {
-//      message: err.message,
-//      error: err
-//    });
-//  });
-//}
-//
-//// production error handler
-//// no stacktraces leaked to user
-//app.use(function(err, req, res, next) {
-//  res.status(err.status || 500);
-//  res.render('error', {
-//    message: err.message,
-//    error: {}
-//  });
-//});
-
-
-
 var express = require('express');
 //var hbs = require('hbs');
 var path = require('path');
@@ -63,7 +16,6 @@ app.use('/', routes);
 
 // partials --> nog niet nodig
 //hbs.registerPartials(__dirname + '/views/partials');
-
 
 app.listen(3000, function () {
   console.log('App listening on port 3000 :)');
