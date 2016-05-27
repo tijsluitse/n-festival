c0d061facc8685ce48198fafa235683b432c2d60
Lisa Klein <lisa.klein@rtl.nl>
2016-05-27 12:39:01 +0200

    copied all files to server

---
+++ app.js
@@ -0,0 +1,72 @@
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
+//
+//
+//// uncomment after placing your favicon in /public
+////app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  var err = new Error('Not Found');
+//  err.status = 404;
+//  next(err);
+//});
+//
+//// error handlers
+//
+//// development error handler
+//// will print stacktrace
+//if (app.get('env') === 'development') {
+//  app.use(function(err, req, res, next) {
+//    res.status(err.status || 500);
+//    res.render('error', {
+//      message: err.message,
+//      error: err
+//    });
+//  });
+//}
+//
+//// production error handler
+//// no stacktraces leaked to user
+//app.use(function(err, req, res, next) {
+//  res.status(err.status || 500);
+//  res.render('error', {
+//    message: err.message,
+//    error: {}
+//  });
+//});
+
+
+
+var express = require('express');
+//var hbs = require('hbs');
+var path = require('path');
+var app = express();
+var routes = require('./routes/index');
+
+// use files in public map -> you can use /css/style.css as links
+app.use(express.static('public'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// app responds to everything after / -> showevents etc.
+app.use('/', routes);
+
+// partials --> nog niet nodig
+//hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000 :)');
+});
+
+module.exports = app;
