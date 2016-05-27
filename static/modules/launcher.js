f07fdf25061f9cb6910c49552c585d706a3ef336
Tijs Luitse <tijs.luitse@gmail.com>
2016-05-27 11:49:25 +0200

    added geolocation and map

--- static/modules/launcher.js
+++ static/modules/launcher.js
@@ -4,6 +4,8 @@ var launcher = (function() {
 
 	var init = function() {
         featureDetect.storage();
+        geolocation.user();
+        geolocation.map();
 		sections.init(); // Routes.js
 	}
 
