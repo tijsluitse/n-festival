41e8c8d8a26659c905a73033ded3b8b97ca7d575
Lisa Klein <lisa.klein@planet.nl>
2016-05-30 12:38:43 +0200

    json data in html

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+// GET data (kan dit in een andere file en data meesturen templates?)
 var fs = require('fs');
 var obj;
 fs.readFile('./data/data.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  obj = JSON.parse(data);
-    
-    console.log(obj[0].curators[0].name);
+    if (err) throw err;
+    obj = JSON.parse(data);
 });
-
-  var names = [
-        {name: 'Lisa'},
-        {name: 'Linda'},
-        {name: 'Tijs'}
-    ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home');
-});
-
-router.get('/showevents', function(req, res, next) {
-    
+router.get('/', function (req, res, next) {
     var data = {obj: obj};
     
-  res.render('showEvents', data);
+    res.render('home', data);
 });
 
-router.get('/timetable', function(req, res, next) {
-  res.render('timeTable');
+router.get('/showevents', function (req, res, next) {  
+    var data = {obj: obj};
+    
+    res.render('showEvents', data);
+});
+
+router.get('/timetable', function (req, res, next) {
+    res.render('timeTable');
 });
 
 module.exports = router;
