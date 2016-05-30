0914770722cebca70915c70de380aa54321e0582
Tijs Luitse <tijs.luitse@gmail.com>
2016-05-30 14:42:25 +0200

    final update staging files

--- public/js/modules/templating.js
+++ public/js/modules/templating.js
@@ -1,7 +1,6 @@
 var templating = (function () {
 
     var init = function (hasStorage) {
-        // var showEvents = document.getElementById('showEvents').innerHTML;
 
         if (hasStorage) {
             var data = JSON.parse(localStorage.getItem('allEventsData'));
@@ -10,13 +9,10 @@ var templating = (function () {
             console.log("There is no localStorage detected.");
         }
 
-        // document.getElementById('showEvents').innerHTML = Mustache.render(showEvents, {
-        //     data: data
-        // });
-
     }
 
     return {
         init: init
     }
+    
 })();
