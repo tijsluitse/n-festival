0a79e07784a8e962fab6cb57ce2f1cf71a481342
Linda <lindavandijkdesign@gmail.com>
2016-05-31 12:33:18 +0200

    Heart icon los getrokken van event
    zodat op beide elementen geklikt kan worden

--- public/js/modules/ux.js
+++ public/js/modules/ux.js
@@ -1,6 +1,6 @@
 var ux = (function () {
 
-    var menuSlide = function () {
+    var menuSlide = function() {
 
         var menu = document.getElementById('menu');
         menu.classList.add('displayNone');
@@ -8,14 +8,14 @@ var ux = (function () {
         var menuButton = document.getElementById('menuButton').onclick = function() {
             menu.classList.toggle('displayNone');
 
-            document.getElementById("menubar1").classList.toggle("animateBar1");
-            document.getElementById("menubar2").classList.toggle("animateBar2");
-            document.getElementById("menubar3").classList.toggle("animateBar3");
+            document.getElementById('menubar1').classList.toggle('animateBar1');
+            document.getElementById('menubar2').classList.toggle('animateBar2');
+            document.getElementById('menubar3').classList.toggle('animateBar3');
         }
     };
     
     var detailPage = function(){
-        var detailTarget = document.querySelector(".discoverEvents");
+        var detailTarget = document.querySelector('.discoverEvents');
         
         function popupDetail (e){
             e.preventDefault();
@@ -25,12 +25,19 @@ var ux = (function () {
             detailTarget.addEventListener('click', popupDetail, false);
         }
         else{
-            detailTarget.attachEvent("onclick", popupDetail);
+            detailTarget.attachEvent('onclick', popupDetail);
+        }
+    };
+
+    var myRoute = function() {
+        var myRouteButton = document.querySelector('.buttonAddToRoute').onclick = function() {
+            var heart = document.querySelector('.eventHeartIcon').classList.toggle('eventAdded');
         }
     };
     
     return {
         menuSlide: menuSlide,
-        detailPage: detailPage
+        detailPage: detailPage,
+        myRoute: myRoute
     }
 })();
