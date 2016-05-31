44d75a49edf49e26829ca62beb71360b61de00a8
Tijs Luitse <tijs.luitse@gmail.com>
2016-05-31 12:16:07 +0200

    ux events from launcher

--- public/js/modules/launcher.js
+++ public/js/modules/launcher.js
@@ -3,10 +3,9 @@
 var launcher = (function() {
 
 	var init = function() {
-		document.getElementById('locationMap').classList.remove('hide');
         featureDetect.storage();
         geolocation.currentPositionMarker();
-		ux.menuSlide();
+        ux.launcher();
 	}
 
 	return {
--- public/js/modules/ux.js
+++ public/js/modules/ux.js
@@ -1,5 +1,18 @@
 var ux = (function () {
 
+    var launcher = function() {
+
+        ux.noJsReset();
+        ux.menuSlide();
+        ux.fullScreenMap();
+
+    };
+
+    var noJsReset = function() {
+        var locationMap = document.getElementById('locationMap');
+        locationMap.classList.remove('hide');
+    };
+
     var menuSlide = function () {
 
         var menu = document.getElementById('menu');
@@ -8,13 +21,28 @@ var ux = (function () {
         var menuButton = document.getElementById('menuButton').onclick = function() {
             menu.classList.toggle('displayNone');
 
-            document.getElementById("menubar1").classList.toggle("animateBar1");
-            document.getElementById("menubar2").classList.toggle("animateBar2");
-            document.getElementById("menubar3").classList.toggle("animateBar3");
+            document.getElementById('menubar1').classList.toggle('animateBar1');
+            document.getElementById('menubar2').classList.toggle('animateBar2');
+            document.getElementById('menubar3').classList.toggle('animateBar3');
         }
+
     };
     
+    var fullScreenMap = function() {
+
+        // var fullMap = document.getElementById('showFullMap');
+        // var locationMap = document.getElementById('locationMap');
+
+        // fullMap.onclick = function() {
+        //     locationMap.classList.toggle('fullscreen');
+        // };
+
+    };
+
     return {
-        menuSlide: menuSlide
+        noJsReset; noJsReset,
+        launcher: launcher,
+        menuSlide: menuSlide,
+        fullScreenMap: fullScreenMap
     }
 })();
