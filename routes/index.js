a0a151d9a1e07d165937b51a466280961952ffeb
Lisa Klein <lisa.klein@planet.nl>
2016-06-07 16:57:02 +0200

    one home template

--- routes/index.js
+++ routes/index.js
@@ -57,14 +57,14 @@ router.get('/', function (req, res, next) {
     if (now < day1) {
         var array = findObject(data, ['info', 'date'], '08-10-2016');
 
-        res.render('homeDay1', {
+        res.render('home', {
             obj: array
         });
 
     } else {
         var array = findObject(data, ['info', 'date'], '09-10-2016');
 
-        res.render('homeDay2', {
+        res.render('home', {
             obj: array
         });
     }
@@ -79,7 +79,7 @@ router.get('/day1', function (req, res, next) {
 
     var array = findObject(data, ['info', 'date'], '08-10-2016');
 
-    res.render('homeDay1', {
+    res.render('home', {
         obj: array
     });
 
@@ -93,7 +93,7 @@ router.get('/day2', function (req, res, next) {
 
     var array = findObject(data, ['info', 'date'], '09-10-2016');
 
-    res.render('homeDay2', {
+    res.render('home', {
         obj: array
     });
 });
