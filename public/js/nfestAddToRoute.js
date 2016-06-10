04d5673b183f9e850d21a291f83ce23a4dd41853
Linda <lindavandijkdesign@gmail.com>
2016-06-10 14:02:23 +0200

    redesign

--- public/js/nfestAddToRoute.js
+++ public/js/nfestAddToRoute.js
@@ -8,11 +8,20 @@ nfest.addToRoute = (function () {
         var add = document.querySelectorAll(".buttonAddToRoute");
 
         for (var i = add.length - 1; i >= 0; i--) {
+
             add[i].onclick = function (evt) {
                 evt.currentTarget.classList.toggle("addedToRoute");
+
+                document.querySelector(".myRouteCounter").classList.toggle("myRouteAdded");
             }
         }
+
+        // add.onclick = function() {
+        //     document.querySelector(".myRouteCounter").classList.add("myRouteAdded");
+        //     console.log('click')
+        // }
     }
+
     
     return{
         buttonToggle: buttonToggle
