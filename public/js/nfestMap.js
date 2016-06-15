690e18b7ea97deab012dd0d41e524907d86ac371
Lisa Klein <lisa.klein@planet.nl>
2016-06-15 18:47:44 +0200

    -

--- public/js/nfestMap.js
+++ public/js/nfestMap.js
@@ -21,15 +21,15 @@ nfest.map = (function () {
         var map = new google.maps.Map(document.getElementById("locationMap"), mapOptions);
 
         nfest.map.jsActivate(map);
-        nfest.map.mapStyle(map);    
-        nfest.map.watchLocation(map);               
+        nfest.map.mapStyle(map);
+        nfest.map.watchLocation(map);
         nfest.map.venueMarkers(map);
         nfest.map.removeVenueMarker(map);
     }
 
     var jsActivate = function (map) {
         var locationMap = document.getElementById('locationMap');
-            locationMap.classList.remove('hide');
+        locationMap.classList.remove('hide');
     }
 
     var mapStyle = function (map) {
@@ -157,24 +157,24 @@ nfest.map = (function () {
     }
 
     // var getLocation = function (map) {
-        
+
     //     if (navigator.geolocation) { // check if geolocation is supported
-            
+
     //         nfest.helpers.storageCheck(function (hasStorage) { // check is LS is supported
-                
+
     //             if (hasStorage) { // LS supported
-                    
+
     //                 if (localStorage.getItem("userCoordinates") === null) { // LS userCoordinates are empty
-                        
+
     //                     navigator.geolocation.getCurrentPosition(success, error);                              
 
     //                     function success(position) {
     //                         var userLatitude = position.coords.latitude,
     //                             userLongitude = position.coords.longitude,
     //                             userCoordinates = [userLatitude, userLongitude];                
-                            
+
     //                         localStorage.setItem('userCoordinates', userCoordinates);
-                            
+
     //                         var userCoordinates = localStorage.getItem('userCoordinates'),
     //                             userC = userCoordinates.split(","),                            
     //                             userLat = parseFloat(userC[0]),
@@ -203,7 +203,7 @@ nfest.map = (function () {
     //                         userLongitude = position.coords.longitude,
     //                         userPosition = new google.maps.LatLng(userLat, userLng);
     //                 };
-    
+
     //                 function error() {
     //                     console.log('Unable to get your position.');                        
     //                     var latLng = new google.maps.LatLng(52.391286, 4.917583); // set map on Amsterdam Noord                    
@@ -218,17 +218,17 @@ nfest.map = (function () {
     //     }
     // }
 
-    var updatePosition = function(marker) {
+    var updatePosition = function (marker) {
 
         function success(position) {
 
             // Get lat, lng from position
             var lat = position.coords.latitude,
                 lng = position.coords.longitude,
-                coords = [lat, lng];                
+                coords = [lat, lng];
 
             // Update the marker with new lat and lng        
-            marker.setPosition(new google.maps.LatLng(lat, lng));               
+            marker.setPosition(new google.maps.LatLng(lat, lng));
         };
 
         function error() {
@@ -244,20 +244,20 @@ nfest.map = (function () {
         navigator.geolocation.watchPosition(success, error, options);
     }
 
-    var watchLocation = function(map) {
+    var watchLocation = function (map) {
 
-        if(navigator.geolocation) {
-            
+        if (navigator.geolocation) {
+
             function success(position) {
-                
+
                 var userLatitude = position.coords.latitude,
                     userLongitude = position.coords.longitude,
-                    userCoordinates = [userLatitude, userLongitude];                
-                
+                    userCoordinates = [userLatitude, userLongitude];
+
                 localStorage.setItem('userCoordinates', userCoordinates);
-                
+
                 var userCoordinates = localStorage.getItem('userCoordinates'),
-                    userC = userCoordinates.split(","),                           
+                    userC = userCoordinates.split(","),
                     userLat = parseFloat(userC[0]),
                     userLng = parseFloat(userC[1]);
 
@@ -282,7 +282,7 @@ nfest.map = (function () {
                 google.maps.event.addListener(marker, 'click', function () {
                     infowindow.setContent(contentString);
                     infowindow.open(map, marker);
-                });                
+                });
 
                 marker.setPosition(new google.maps.LatLng(userLat, userLng));
                 map.setCenter(marker.getPosition());
@@ -309,72 +309,73 @@ nfest.map = (function () {
 
     var venueMarkers = function (map) {
 
-        var all = document.querySelectorAll('.addedToRoute');      
-        
+        var all = document.querySelectorAll('.addedToRoute');
+
         Array.prototype.forEach.call(all, function (venue) {
-            venue.addEventListener('click', function(){                
+            venue.addEventListener('click', function () {
                 location.reload();
             });
         });
-        
+
 
         nfest.helpers.getVenueLocations(function (mapLocations, data) {
             var myRouteElements = JSON.parse(localStorage.getItem('myRouteEvents')),
                 infowindow = new google.maps.InfoWindow();
 
-                if (myRouteElements.length == 0) {
-                    // no items in array
-                } else {
-                    var items = document.querySelectorAll('.eventObj.myRouteEvents');                                      
-                    items.forEach(function(item){                    
-                        mapLocations.forEach(function(location){
-                            if (item.dataset.location == location.link) {
-                                var marker = new google.maps.Marker({
-                                    position: {
-                                        lat: parseFloat(location.lat),
-                                        lng: parseFloat(location.lng)
-                                    },
-                                    map: map,
-                                    animation: google.maps.Animation.DROP,
-                                    icon: "/img/location.png",
-                                    optimized: true,
-                                    title: "First Infowindow!"
-                                });
+            if (myRouteElements.length == 0) {
+                // no items in array
+            } else {
+                var items = document.querySelectorAll('.eventObj.myRouteEvents');
 
-                                var locationLink = location.link;
+                Array.prototype.forEach.call(items, function (item) {
+                    mapLocations.forEach(function (location) {
+                        if (item.dataset.location == location.link) {
+                            var marker = new google.maps.Marker({
+                                position: {
+                                    lat: parseFloat(location.lat),
+                                    lng: parseFloat(location.lng)
+                                },
+                                map: map,
+                                animation: google.maps.Animation.DROP,
+                                icon: "/img/location.png",
+                                optimized: true,
+                                title: "First Infowindow!"
+                            });
 
-                                var link = '<a href="/location/' + locationLink + '" class="popupButton">';
-                                var routeLink = '<a href="https://maps.google.com?saddr=Current+Location&daddr=' + location.lat + ',' + location.lng + '" class="popupButton buttonRoute">';
+                            var locationLink = location.link;
 
-                                var contentString =
-                                    '<div id="content">' +
-                                    '<h1>' +
-                                    location.title +
-                                    '</h1>' +
-                                    '<p>' +
-                                    location.address +
-                                    '</p>' +
-                                    '<div class="popupButtons ">' +
-                                    routeLink +
-                                    'Route' +
-                                    '</a>' +
-                                    link +
-                                    'Evenementen' +
-                                    '</a>' +
-                                    '</div>' +
-                                    '</div>';
+                            var link = '<a href="/location/' + locationLink + '" class="popupButton">';
+                            var routeLink = '<a href="https://maps.google.com?saddr=Current+Location&daddr=' + location.lat + ',' + location.lng + '" class="popupButton buttonRoute">';
 
-                                google.maps.event.addListener(marker, 'click', function () {
-                                    infowindow.setContent(contentString);
-                                    infowindow.open(map, marker);
-                                });
+                            var contentString =
+                                '<div id="content">' +
+                                '<h1>' +
+                                location.title +
+                                '</h1>' +
+                                '<p>' +
+                                location.address +
+                                '</p>' +
+                                '<div class="popupButtons ">' +
+                                routeLink +
+                                'Route' +
+                                '</a>' +
+                                link +
+                                'Evenementen' +
+                                '</a>' +
+                                '</div>' +
+                                '</div>';
 
-                                marker.setMap(map);
-                            }
-                        });
-                    });    
+                            google.maps.event.addListener(marker, 'click', function () {
+                                infowindow.setContent(contentString);
+                                infowindow.open(map, marker);
+                            });
 
-                }
+                            marker.setMap(map);
+                        }
+                    });
+                });
+
+            }
 
         });
 
@@ -384,46 +385,46 @@ nfest.map = (function () {
 
         //     locationMarkers.forEach(function (location) {
 
-                // var marker = new google.maps.Marker({
-                //     position: {
-                //         lat: parseFloat(location.lat),
-                //         lng: parseFloat(location.lng)
-                //     },
-                //     map: map,
-                //     icon: '/img/location.png',
-                //     optimized: true,
-                //     title: 'First Infowindow!'
-                // });
+        // var marker = new google.maps.Marker({
+        //     position: {
+        //         lat: parseFloat(location.lat),
+        //         lng: parseFloat(location.lng)
+        //     },
+        //     map: map,
+        //     icon: '/img/location.png',
+        //     optimized: true,
+        //     title: 'First Infowindow!'
+        // });
 
-                // var locationLink = location.link;
+        // var locationLink = location.link;
 
-                // var link = '<a href="/location/' + locationLink + '" class="popupButton">';
-                // var routeLink = '<a href="https://maps.google.com?saddr=Current+Location&daddr=' + location.lat + ',' + location.lng + '" class="popupButton buttonRoute">';
+        // var link = '<a href="/location/' + locationLink + '" class="popupButton">';
+        // var routeLink = '<a href="https://maps.google.com?saddr=Current+Location&daddr=' + location.lat + ',' + location.lng + '" class="popupButton buttonRoute">';
 
-                // var contentString =
-                //     '<div id="content">' +
-                //     '<h1>' +
-                //     location.title +
-                //     '</h1>' +
-                //     '<p>' +
-                //     location.address +
-                //     '</p>' +
-                //     '<div class="popupButtons ">' +
-                //     routeLink +
-                //     'Route' +
-                //     '</a>' +
-                //     link +
-                //     'Evenementen' +
-                //     '</a>' +
-                //     '</div>' +
-                //     '</div>';
+        // var contentString =
+        //     '<div id="content">' +
+        //     '<h1>' +
+        //     location.title +
+        //     '</h1>' +
+        //     '<p>' +
+        //     location.address +
+        //     '</p>' +
+        //     '<div class="popupButtons ">' +
+        //     routeLink +
+        //     'Route' +
+        //     '</a>' +
+        //     link +
+        //     'Evenementen' +
+        //     '</a>' +
+        //     '</div>' +
+        //     '</div>';
 
-                // google.maps.event.addListener(marker, 'click', function () {
-                //     infowindow.setContent(contentString);
-                //     infowindow.open(map, marker);
-                // });
+        // google.maps.event.addListener(marker, 'click', function () {
+        //     infowindow.setContent(contentString);
+        //     infowindow.open(map, marker);
+        // });
 
-                // marker.setMap(map);
+        // marker.setMap(map);
 
         //     });
         // });
@@ -443,9 +444,9 @@ nfest.map = (function () {
     return {
         mapLauncher: mapLauncher,
         jsActivate: jsActivate,
-        mapStyle: mapStyle,            
+        mapStyle: mapStyle,
         updatePosition: updatePosition,
-        watchLocation: watchLocation,    
+        watchLocation: watchLocation,
         venueMarkers: venueMarkers,
         removeVenueMarker: removeVenueMarker
     }
