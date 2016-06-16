96e2c847f4a4868d53561b922a1b9fe800d67408
Tijs Luitse <tijs.luitse@gmail.com>
2016-06-16 14:38:04 +0200

    menu update

--- public/js/nfestMenu.js
+++ public/js/nfestMenu.js
@@ -9,7 +9,6 @@ nfest.menu = (function () {
     var menuLauncher = function () {
         nfest.menu.menuScrollTop();
         nfest.menu.menuFunctions();
-        console.log("hoi")
     }; 
 
     var nLogo = document.getElementById('nLogo');
@@ -20,114 +19,52 @@ nfest.menu = (function () {
 
     var menuScrollTop = function () {
 
-        var scrollToMenu =  function () {
+        // code by: http://jsfiddle.net/62MTU/15/
+        function scrollTo(element, to, duration, time) {                    
+            var start = element.scrollTop,
+                change = to - start,
+                currentTime = 0,
+                increment = 40;
 
-            // code by: http://jsfiddle.net/62MTU/15/
-            function scrollTo(element, to, duration) {                    
-                var start = element.scrollTop,
-                    change = to - start,
-                    currentTime = 0,
-                    increment = 40;
-                    
-                var animateScroll = function(){        
-                    currentTime += increment;
-                    var val = Math.easeInOutQuad(currentTime, start, change, duration);
-                    element.scrollTop = val;
-                    if(currentTime < duration) {
-                        setTimeout(animateScroll, increment);
-                    }
-                };
-                animateScroll();
-            }
-
-            //t = current time, b = start value, c = change in value, d = duration
-            Math.easeInOutQuad = function (t, b, c, d) {
-                t /= d/1;
-                if (t < 1) return c/2*t*t + b;
-                t--;
-                return -c/2 * (t*(t-2) - 1) + b;
-            };  
-
-            scrollTo(document.body, 1000, 1000);  
-
+                // console.log(time);
+                
+            var animateScroll = function(){        
+                currentTime += increment;
+                var val = Math.easeInOutQuad(currentTime, start, change, duration);
+                element.scrollTop = val;
+                if(currentTime < duration) {
+                    setTimeout(animateScroll, increment);
+                }
+            };
+            animateScroll();
         }
 
-        var scrollToMenuFast =  function () {
+        //t = current time, b = start value, c = change in value, d = duration
+        Math.easeInOutQuad = function (t, b, c, d) {
+            t /= d/2;
+            if (t < 1) return c/2*t*t + b;
+            t--;
+            return -c/2 * (t*(t-2) - 1) + b;
+        };
 
-            // code by: http://jsfiddle.net/62MTU/15/
-            function scrollTo(element, to, duration) {                    
-                var start = element.scrollTop,
-                    change = to - start,
-                    currentTime = 0,
-                    increment = 40;
-                    
-                var animateScroll = function(){        
-                    currentTime += increment;
-                    var val = Math.easeInOutQuad(currentTime, start, change, duration);
-                    element.scrollTop = val;
-                    if(currentTime < duration) {
-                        setTimeout(animateScroll, increment);
-                    }
-                };
-                animateScroll();
-            }
-
-            //t = current time, b = start value, c = change in value, d = duration
-            Math.easeInOutQuad = function (t, b, c, d) {
-                t /= d/2;
-                if (t < 1) return c/2*t*t + b;
-                t--;
-                return -c/2 * (t*(t-2) - 1) + b;
-            };  
-
-            scrollTo(document.body, 1000, 0);  
-
+        if (localStorage.getItem('firstTime') === 'firstTime') {
+            console.log("vol");
+            var height = document.querySelector('#aboutSection').offsetHeight;
+            scrollTo(document.body, height, 0, "second"); 
         }
-
-        var scrollToAbout =  function () {
-
-            // code by: http://jsfiddle.net/62MTU/15/
-            function scrollTo(element, to, duration) {                    
-                var start = element.scrollTop,
-                    change = to - start,
-                    currentTime = 0,
-                    increment = 40;
-                    
-                var animateScroll = function(){        
-                    currentTime += increment;
-                    var val = Math.easeInOutQuad(currentTime, start, change, duration);
-                    element.scrollTop = val;
-                    if(currentTime < duration) {
-                        setTimeout(animateScroll, increment);
-                    }
-                };
-                animateScroll();
-            }
-
-            //t = current time, b = start value, c = change in value, d = duration
-            Math.easeInOutQuad = function (t, b, c, d) {
-                t /= d/2;
-                if (t < 1) return c/2*t*t + b;
-                t--;
-                return -c/2 * (t*(t-2) - 1) + b;
-            };  
-
-            scrollTo(document.body, 0, 1000);  
-
+        
+        if (localStorage.getItem('firstTime') === null) { 
+            // console.log("leeg");
+            var height = document.querySelector('#aboutSection').offsetHeight;            
+            scrollTo(document.body, height, 1250, "first"); 
+            localStorage.setItem('firstTime', 'firstTime');
         }
 
         nLogo.addEventListener('click', function(){
-            scrollToAbout();
+            var height = document.querySelector('#aboutSection').offsetHeight;
+            scrollTo(document.body, -height, 1250, "click");
         });
-
-        if (document.cookie.indexOf("visited") >= 0) { // Second Time            
-            scrollToMenuFast();
-        } else { // First Time    
-            scrollToMenu();             
-            document.cookie = "visited=yes; expires=Fri, 17 Jun 2016 12:00:00 UTC;";                       
-        }
-
-    };
+    }
 
     return {
         menuLauncher: menuLauncher,
