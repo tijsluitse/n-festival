90129340d25fa932a3f509c808ac1002a8cb41bc
Lisa Klein <lisa.klein@planet.nl>
2016-06-20 15:54:02 +0200

    show bikedist when no local storage

--- public/js/nfestLocation.js
+++ public/js/nfestLocation.js
@@ -10,10 +10,10 @@ nfest.location = (function () {
         nfest.location.getUserLocation();
         nfest.location.watchLocation();
 
-        if (window.location.pathname == '/program' || '/day1' || '/day2' || '/location' || '/myroute' || '/discover') {            
-            nfest.location.eventDistance();            
+        if (window.location.pathname == '/program' || '/day1' || '/day2' || '/location' || '/myroute' || '/discover') {
+            nfest.location.eventDistance();
         }
-        if (window.location.pathname == '/detail/:id') {            
+        if (window.location.pathname == '/detail/:id') {
             nfest.location.eventDistance();
         }
 
@@ -24,7 +24,7 @@ nfest.location = (function () {
         var removeBikeDist = function () {
             if (window.location.pathname == '/program' || '/day1' || '/day2' || '/location' || '/myroute' || '/detail' || 'discover') {
                 var bikeDist = document.querySelectorAll('.eventDistance');
-                
+
                 Array.prototype.forEach.call(bikeDist, function (item) {
                     item.classList.add('hide');
                 });
@@ -63,7 +63,7 @@ nfest.location = (function () {
         }
 
         // check if geolocation is supported
-        if (navigator.geolocation) {            
+        if (navigator.geolocation) {
             nfest.helpers.storageCheck(function (hasStorage) {
                 if (hasStorage) {
                     if (localStorage.getItem('userCoordinates') === null) {
@@ -81,7 +81,7 @@ nfest.location = (function () {
                             removeBikeDist();
                         };
 
-                    } 
+                    }
                 } else {
                     navigator.geolocation.getCurrentPosition(success, error);
 
@@ -136,58 +136,78 @@ nfest.location = (function () {
             }, 10000);
         });
 
-        var calculateDist = function (data) {        
+        var calculateDist = function (data) {
+            var eventList = document.querySelectorAll('.eventObj'),
+                allDistances = [];
 
-            var userCoordinates = localStorage.getItem('userCoordinates'),
-                userC = userCoordinates.split(','),
-                userLat = parseFloat(userC[0]),
-                eventList = document.querySelectorAll('.eventObj'),
-                userLng = parseFloat(userC[1]),
-                allDistances = [];         
+            if (localStorage.getItem('userCoordinates') === null) {
+                navigator.geolocation.getCurrentPosition(success);
 
-            Array.prototype.forEach.call(eventList, function (event) {
-                var location = event.dataset.location;                
-                for (var i = 0; i < data.length; i++) { 
-                    var id = data[i].slug,
-                        uLat = userLat,
-                        uLng = userLng,
-                        lat = data[i].acf.location.lat,
-                        lng = data[i].acf.location.lng;                
-                    if (location === id) {
-                        distance(id, uLat, uLng, lat, lng, event);
+                function success(position) {
+                    var userLat = position.coords.latitude,
+                        userLng = position.coords.longitude,
+                        userCoordinates = [userLat, userLng];
+                    localStorage.setItem('userCoordinates', userCoordinates);
+
+                    calculate(userLat, userLng);
+                };
+            } else {
+                var userCoordinates = localStorage.getItem('userCoordinates'),
+                    userC = userCoordinates.split(','),
+                    userLat = parseFloat(userC[0]),
+                    userLng = parseFloat(userC[1]);
+
+                calculate(userLat, userLng);
+            }
+
+
+            function calculate(userLat, userLng) {
+                Array.prototype.forEach.call(eventList, function (event) {
+                    var location = event.dataset.location;
+                    for (var i = 0; i < data.length; i++) {
+                        var id = data[i].slug,
+                            uLat = userLat,
+                            uLng = userLng,
+                            lat = data[i].acf.location.lat,
+                            lng = data[i].acf.location.lng;
+                        if (location === id) {
+                            distance(id, uLat, uLng, lat, lng, event);
+                        }
                     }
-                }
-            });
-
-            function distance(id, lat1, lon1, lat2, lon2, event) {                
-                var unit = 'K',
-                    radlat1 = Math.PI * lat1 / 180,
-                    radlat2 = Math.PI * lat2 / 180,
-                    theta = lon1 - lon2,
-                    radtheta = Math.PI * theta / 180,
-                    dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta),
-                    dist = Math.acos(dist),
-                    dist = dist * 180 / Math.PI,
-                    dist = dist * 60 * 1.1515;
-                if (unit == 'K') {
-                    dist = dist * 1.609344
-                }
-                if (unit == 'N') {
-                    dist = dist * 0.8684
-                }
-
-                var result = dist.toFixed(2),
-                    bikeTime = 6 * result,
-                    string = '.bikeDist';
-                    bikeTime = bikeTime.toFixed(0);
-
-                allDistances.push({
-                    distance: result
                 });
 
-                event.querySelector(string).innerHTML = bikeTime + ' min';
+                function distance(id, lat1, lon1, lat2, lon2, event) {
+                    var unit = 'K',
+                        radlat1 = Math.PI * lat1 / 180,
+                        radlat2 = Math.PI * lat2 / 180,
+                        theta = lon1 - lon2,
+                        radtheta = Math.PI * theta / 180,
+                        dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta),
+                        dist = Math.acos(dist),
+                        dist = dist * 180 / Math.PI,
+                        dist = dist * 60 * 1.1515;
+                    if (unit == 'K') {
+                        dist = dist * 1.609344
+                    }
+                    if (unit == 'N') {
+                        dist = dist * 0.8684
+                    }
 
+                    var result = dist.toFixed(2),
+                        bikeTime = 6 * result,
+                        string = '.bikeDist';
+                    bikeTime = bikeTime.toFixed(0);
+
+                    allDistances.push({
+                        distance: result
+                    });
+
+                    event.querySelector(string).innerHTML = bikeTime + ' min';
+
+                }
             }
+
+
         }
 
     }
