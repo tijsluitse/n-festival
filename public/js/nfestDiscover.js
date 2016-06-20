6776f44dae1d6dcb491efe96a671ee75a8208f55
Lisa Klein <lisa.klein@planet.nl>
2016-06-20 14:04:41 +0200

    discover events

--- public/js/nfestDiscover.js
+++ public/js/nfestDiscover.js
@@ -2,57 +2,82 @@ var nfest = nfest || {};
 'use strict';
 
 nfest.discover = (function () {
-
     var discoverEvents = document.querySelectorAll('.eventDiscover'),
         discoverButton = document.getElementById('discoverBttn');
 
-
-    Array.prototype.forEach.call(discoverEvents, function (event) {
-        event.classList.add('hide');
-    });
-    
-    var flash = setInterval(function () {
-            var discoverEvents = document.querySelectorAll('.eventDiscover');
-
-            Array.prototype.forEach.call(discoverEvents, function (event) {
-                event.classList.add('hide');
-            });
-
-            var item = discoverEvents[Math.floor(Math.random() * discoverEvents.length)];
-
-            item.classList.remove('hide');
-
-
-        }, 100);
-
-
-    if (discoverButton.addEventListener) {
-        discoverButton.addEventListener('mousedown', flashEvents);
-    } else {
-        discoverButton.attachEvent('onmousedown', flashEvents);
+    var discoverLauncher = function () {
+        nfest.discover.showRandomEvent();
+        nfest.discover.mouseTouchEvents();
     }
 
-    function flashEvents() {
+    var showRandomEvent = function () {
+        Array.prototype.forEach.call(discoverEvents, function (event) {
+            event.classList.add('hide');
+        });
 
-        flash();
-    }
-    
-    
-     if (discoverButton.addEventListener) {
-        discoverButton.addEventListener('mouseup', stopFlash);
-    } else {
-        discoverButton.attachEvent('onmouseup', stopFlash);
-    }
-    
-    function stopFlash (){
-        clearInterval(flash);
+        var item = discoverEvents[Math.floor(Math.random() * discoverEvents.length)];
+
+        item.classList.remove('hide');
     }
 
+    var mouseTouchEvents = function () {
+        var int = null;
 
+        //mouse events when used on desktop
+        discoverButton.onmousedown = function () {
+            int = setInterval(function () {
+                var discoverEvents = document.querySelectorAll('.eventDiscover');
 
+                Array.prototype.forEach.call(discoverEvents, function (event) {
+                    event.classList.add('hide');
+                });
 
+                var item = discoverEvents[Math.floor(Math.random() * discoverEvents.length)];
 
+                item.classList.remove('hide');
 
+            }, 50);
+        }
+
+        discoverButton.onmouseup = function () {
+            clearInterval(int);
+            int = null;
+        };
+
+        discoverButton.onmouseleave = function () {
+            clearInterval(int);
+            int = null;
+        };
+
+        // touch events when used on phone
+        discoverButton.ontouchstart = function () {
+            int = setInterval(function () {
+                var discoverEvents = document.querySelectorAll('.eventDiscover');
+
+                Array.prototype.forEach.call(discoverEvents, function (event) {
+                    event.classList.add('hide');
+                });
+
+                var item = discoverEvents[Math.floor(Math.random() * discoverEvents.length)];
+
+                item.classList.remove('hide');
+
+            }, 50);
+        }
+
+        discoverButton.ontouchend = function () {
+            clearInterval(int);
+            int = null;
+        };
+    }
+
+    return{
+        discoverLauncher: discoverLauncher,
+        showRandomEvent: showRandomEvent,
+        mouseTouchEvents: mouseTouchEvents
+    }
 
 
 })();
+
+nfest.discover.discoverLauncher();
