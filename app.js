ac908162339ce26280ce5050502790cb1793ef75
Tijs Luitse <tijs.luitse@gmail.com>
2016-06-21 12:53:10 +0200

    checked al js and html files

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-var express = require('express');
-var hbs = require('hbs');
-var path = require('path');
-var app = express();
-var routes = require('./routes/index');
+/* Requires */
+var express = require('express'),
+	hbs = require('hbs'),
+	path = require('path'),
+	app = express(),
+	routes = require('./routes/index');
 
-
-app.use(function(req, res, next) {
-   //make sure that people can get and post data by allowing cross origin
+/* Make sure that people can get and post data by allowing cross origin */
+app.use(function(req, res, next) {   	
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-// use files in public map -> you can use /css/style.css as links
+/* Use files in public map -> you can use /css/style.css as links */
 app.use(express.static(path.join(__dirname, 'public')));
 
-// view engine setup
+/* View engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// app responds to everything after / -> showevents etc.
+/* App responds to everything after / -> showevents etc. */
 app.use('/', routes);
 
-// partials --> nog niet nodig
+/* Partials --> nog niet nodig */
 hbs.registerPartials(__dirname + '/views/partials');
 
+/* Binds app to localhost:3000 in browser */
 app.listen(3000, function () {
-  console.log('App listening on port 3000 :)');
+  	console.log('App listening on port 3000 :)');
 });
 
 module.exports = app;
